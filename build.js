99f0c6403b611c9bd77da04a44a1bce792167ae6
stel1os <stel1os@users.noreply.github.com>
2026-05-20 00:11:14 +0300

    Fix: prepend v prefix to APP_VERSION in build output

--- build.js
+++ build.js
@@ -12,7 +12,7 @@ const js = [
 let html = fs.readFileSync('src/index.html', 'utf8');
 html = html.replace('<!-- STYLE -->', '<style>\n' + css + '\n</style>');
 html = html.replace('<!-- SCRIPTS -->', '<script>\n' + js + '\n</script>');
-html = html.replace('__APP_VERSION__', version);
+html = html.replace('__APP_VERSION__', 'v' + version);
 
 fs.writeFileSync('loan-tracker.html', html, 'utf8');
 console.log('Built loan-tracker.html (v' + version + ')');
